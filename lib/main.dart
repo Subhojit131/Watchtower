273917ee4c4a52fa393cab6f75c75b9e85bd4e86
Subhojit131 <subhojitmandal147@gmail.com>
2025-06-21 00:41:28 +0530

    corrected .env setup

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'screens/scan_link.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
-  await dotenv.load();
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
